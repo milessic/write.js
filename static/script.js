360c48fdd59aecf8046ad098b2516d8d5f78d7d2
Miłosz Jura <milosz.m.jura@gmail.com>
2025-04-01 22:50:47 +0200

    fixed insert link

--- static/script.js
+++ static/script.js
@@ -108,7 +108,7 @@ document.getElementById("tb-btn-code").addEventListener("click", () => { formatT
 document.getElementById("tb-btn-bold").addEventListener("click", () => { formatText("bold") } );
 document.getElementById("tb-btn-italic").addEventListener("click", () => { formatText("italic") } );
 document.getElementById("tb-btn-underline").addEventListener("click", () => { formatText("underline") } );
-document.getElementById("tb-btn-link").addEventListener("click", () => { formatText("link") } );
+document.getElementById("tb-btn-link").addEventListener("click", () => { formatText("a") } );
 document.getElementById("tb-btn-softreturn").addEventListener("click", () => { formatText("softreturn") } );
 document.getElementById("tb-btn-inserttab").addEventListener("click", () => { formatText("inserttab") } );
 
@@ -148,6 +148,8 @@ function formatText(command) {
 		insertTab();
 	} else if ( command === "checkbox" ) {
 		insertCheckbox();
+	} else {
+		console.error("Not supported command - ", command);
 	}
 	focusEditor();
 	performAutoSave();
@@ -1454,4 +1456,12 @@ function loadNotebookFromJson(json, excludeCurrentDocument=true, loadLastOpened=
 	if (loadLastOpened){
 		loadLastOpenedDocument(excludeCurrentDocument, loadLastOpened);
 	}
+}
+
+function createFlashcardByQuerySelector(selector, text1="key",text2="value"){
+	const e = document.createElement("div");
+	e.classList.add("flashcard");
+	e.innerHTML = `<flashcard><key class='key'>${text1}</key><value class='val'>${text2}</value></flashcard>
+	`
+	document.querySelector(selector).appendChild(e)
 }
