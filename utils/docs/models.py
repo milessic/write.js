e3698e0861727097c8fdb68669b5a2e61637799c
Miłosz Jura <milosz.m.jura@gmail.com>
2025-04-09 01:11:22 +0200

    Added Document models for create and update

--- utils/docs/models.py
+++ utils/docs/models.py
@@ -1,6 +1,31 @@
 from pydantic import BaseModel
 from typing import Optional
 
+class DocumentCreateModel(BaseModel):
+    doc_name: str
+    dir: str
+    share_type_id: int
+    content: str
+
+class DocumentUpdateModel(BaseModel):
+    doc_name: Optional[str] = None
+    dir : Optional[str] = None
+    share_type_id: Optional[str] = None
+    content: Optional[str] = None
+
+class DocumentModel(BaseModel):
+    r"""For create and update"""
+    # documents
+    doc_id: int | None
+    doc_name: str
+    doc_created_at: str
+    share_type_id: int
+
+    # revisions
+    version_id: int
+    content: str
+    revision_created_at: str
+
 
 class PortfolioUpdateModel(BaseModel):
     doc_id: int
